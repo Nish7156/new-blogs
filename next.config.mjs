ff5c068a4567c5300c6eabc9ed2ddc803c0fbfd7
Your Name <your.email@example.com>
2024-02-19 00:07:15 +0530

    fwef

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-import withSerwistInit from "@serwist/next";
-
-const withSerwist = withSerwistInit({
-  cacheOnFrontEndNav: true,
-  swSrc: "src/sw.ts", // add the path where you create sw.ts
-  swDest: "public/sw.js",
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development", // to disable pwa in development
-});
-
 const nextConfig = {
-  swcMinify: true,
-  images: {
-    domains: ["cdn.dummyjson.com"],
-  },
+    images: {
+        domains: ['cdn.dummyjson.com'],
+      },
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig;
