724448849f13a7600f06589641143fad4cf9f45d
Rahul Gupta <rahul123alok@gmail.com>
2024-02-24 00:06:56 +0530

    added route for trending-blog

--- src/app/api/trending-blog/route.ts
+++ src/app/api/trending-blog/route.ts
@@ -10,19 +10,19 @@ export async function GET(request: any) {
     const blogs = await db.collection("blogs").find({}).toArray();
 
     // Store the last 20 objects in an array
-    const last10Blogs = blogs.slice(-10);
+    const last20Blogs = blogs.slice(-20);
 
     // Generate 5 random indices
     const randomIndices: number[] = [];
-    while (randomIndices.length < 5) {
-      const randomIndex = Math.floor(Math.random() * last10Blogs.length);
+    while (randomIndices.length < 3) {
+      const randomIndex = Math.floor(Math.random() * last20Blogs.length);
       if (!randomIndices.includes(randomIndex)) {
         randomIndices.push(randomIndex);
       }
     }
 
     // Retrieve the 5 random objects from the array
-    const randomBlogs = randomIndices.map((index) => last10Blogs[index]);
+    const randomBlogs = randomIndices.map((index) => last20Blogs[index]);
 
     return NextResponse.json(randomBlogs);
   } catch (error) {
