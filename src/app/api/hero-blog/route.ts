859b98c756906906f4b0f5eab2d49cccd6dd6bb1
Rahul Gupta <rahul123alok@gmail.com>
2024-02-23 22:19:17 +0530

    added route of hero-blog, return 5 random array from latest 20

---
+++ src/app/api/hero-blog/route.ts
@@ -0,0 +1,32 @@
+import clientPromise from "../../backend/database";
+import { NextResponse } from "next/server";
+
+export async function GET(request: any) {
+  try {
+    const client: any = await clientPromise;
+    const db = client.db();
+
+    // Retrieve the last 20 objects from the database
+    const blogs = await db.collection("blogs").find({}).toArray();
+
+    // Store the last 20 objects in an array
+    const last20Blogs = blogs.slice(-20);
+
+    // Generate 5 random indices
+    const randomIndices: number[] = [];
+    while (randomIndices.length < 5) {
+      const randomIndex = Math.floor(Math.random() * last20Blogs.length);
+      if (!randomIndices.includes(randomIndex)) {
+        randomIndices.push(randomIndex);
+      }
+    }
+
+    // Retrieve the 5 random objects from the array
+    const randomBlogs = randomIndices.map((index) => last20Blogs[index]);
+
+    return NextResponse.json(randomBlogs);
+  } catch (error) {
+    console.error("Error fetching blogs from MongoDB:", error);
+    return NextResponse.json({ message: "Internal server error" });
+  }
+}
