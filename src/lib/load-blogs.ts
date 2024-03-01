cd8ce32f2f4d591efc51ca6a344f0e9aafb8da02
Your Name <your.email@example.com>
2024-03-01 16:52:55 +0530

    ddd

--- src/lib/load-blogs.ts
+++ src/lib/load-blogs.ts
@@ -1,30 +1,29 @@
 import { checkEnvironment } from "@/components/Utilty/checkEnvironment ";
 
 export async function getHeroBlogs() {
-    try {
-      const res = await fetch(`${checkEnvironment()}/api/hero-blog`, {
-        cache: "force-cache",
-      });
-      if (!res.ok) {
-        console.log("error");
-      }
-      return res.json();
-    } catch (error) {
-      console.log(error);
+  try {
+    const res = await fetch(`${checkEnvironment()}/api/hero-blog`, {
+      cache: "force-cache",
+    });
+    if (!res.ok) {
+      console.log("error -> getHeroBlogs");
     }
+    return res.json();
+  } catch (error) {
+    console.log(error);
   }
+}
 
-
-  export async function getEditoresBlogs() {
-    try {
-      const res = await fetch(`${checkEnvironment()}/api/blogs`, {
-        cache: "force-cache",
-      });
-      if (!res.ok) {
-        console.log("error");
-      }
-      return res.json();
-    } catch (error) {
-      console.log(error);
+export async function getEditoresBlogs() {
+  try {
+    const res = await fetch(`${checkEnvironment()}/api/blogs`, {
+      cache: "force-cache",
+    });
+    if (!res.ok) {
+      console.log("error -> getEditoresBlogs");
     }
+    return res.json();
+  } catch (error) {
+    console.log(error);
   }
+}
